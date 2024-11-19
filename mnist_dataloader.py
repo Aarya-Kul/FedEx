83e01390109bd7c81013517200df171a804e86e4
Aarya Kulshrestha <akulshre@umich.edu>
2024-11-18 22:54:24 -0500

    fixed the non_iid dataloader

--- mnist_dataloader.py
+++ mnist_dataloader.py
@@ -4,20 +4,18 @@ from collections import defaultdict
 from torch.utils.data import DataLoader
 from torch.utils.data import DataLoader
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 
-# transform = transforms.Compose([transforms.ToTensor()])
-# # 60,000 pictures 
-# train_mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+transform = transforms.Compose([transforms.ToTensor()])
+# # 60,000 pictures
+train_mnist_data = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 # # 10,000 pictures
-# test_mnist_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+test_mnist_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
 
 
-# Helper function to split indices into training and validation sets
+# Splits indices into training and validation sets
 def split_indices(num_samples, val_ratio=0.2):
-    """Splits indices into training and validation sets."""
     indices = np.random.permutation(num_samples)
     val_size = int(num_samples * val_ratio)
     val_indices = indices[:val_size]
@@ -34,17 +32,20 @@ def create_iid_split(data, num_clients=100):
 
 # Create Non-IID Split
 def create_non_iid_split(data, num_clients=100, shards_per_client=2, shard_size=300):
-    num_classes = 10  # assuming the dataset has 10 classes (e.g., MNIST)
+    num_classes = 10
     class_indices = defaultdict(list)
 
     # Sort indices by digit label
     for idx, (_, label) in enumerate(data):
         class_indices[label].append(idx)
 
+    # print(class_indices.keys())
+
     # Create shards
     shards = []
     for label in range(num_classes):
         np.random.shuffle(class_indices[label])  # Shuffle indices of each class
+        shard_size = min(len(class_indices[label]) - 1, shard_size)
         for i in range(0, len(class_indices[label]), shard_size):
             shards.append(class_indices[label][i:i + shard_size])
 
@@ -59,14 +60,6 @@ def create_non_iid_split(data, num_clients=100, shards_per_client=2, shard_size=
 
 class MNISTDataloader(DataLoader):
     def __init__(self, dataset, num_clients=100, val_ratio=0.0, shard_size=300, is_iid=True):
-        """
-        Args:
-            dataset: The full dataset (e.g., MNIST) to split.
-            num_clients: Number of clients to create for the federated learning scenario.
-            val_ratio: Proportion of data to use for validation.
-            shard_size: Size of shards for the non-IID split.
-            is_iid: Boolean flag to indicate if the split should be IID or non-IID.
-        """
         self.dataset = dataset
         self.num_clients = num_clients
         self.val_ratio = val_ratio
@@ -74,11 +67,8 @@ class MNISTDataloader(DataLoader):
         self.is_iid = is_iid
 
         # Split dataset into train and validation sets
-        # X_train, X_test, y_train, y_val = train_test_split(self.dataset[0], self.dataset[1], test_size=0.2, random_state=42)
-
         self.train_indices, self.val_indices = split_indices(len(self.dataset), self.val_ratio)
-        # self.train_indices, self.val_indices = split_indices(10000, self.val_ratio)
-
+        # self.train_indices, self.val_indices = split_indices(1000, self.val_ratio)
         self.train_data = [(self.dataset[i][0], self.dataset[i][1]) for i in self.train_indices]
         self.val_data = [(self.dataset[i][0], self.dataset[i][1]) for i in self.val_indices]
 
@@ -88,30 +78,32 @@ class MNISTDataloader(DataLoader):
         else:
             self.client_data = create_non_iid_split(self.train_data, self.num_clients, shard_size=self.shard_size)
 
+    # Returns the data assigned to a particular client
     def get_client_data(self, client_id):
-        """Returns the data assigned to a particular client."""
         return self.client_data[client_id]
 
+    # Returns the full training data
     def get_train_data(self):
-        """Returns the full training data."""
         return self.train_data
 
+    # Returns the validation data
     def get_val_data(self):
-        """Returns the validation data."""
         return self.val_data
 
+    # Returns a DataLoader for a particular client
     def get_dataloader(self, client_id, batch_size=32, shuffle=True):
-        """Returns a DataLoader for a particular client."""
         client_indices = self.client_data[client_id]
-        client_dataset = torch.utils.data.Subset(self.dataset, client_indices)
+        if not self.is_iid:
+          client_indices = [item for shard in client_indices for item in shard]
+        client_dataset = torch.utils.data.Subset(self.train_data, client_indices)
         return DataLoader(client_dataset, batch_size=batch_size, shuffle=shuffle)
 
+    # Returns a DataLoader for the full training data
     def get_full_train_dataloader(self, batch_size=32, shuffle=True):
-        """Returns a DataLoader for the full training data."""
         return DataLoader(self.train_data, batch_size=batch_size, shuffle=shuffle)
 
+    # Returns a DataLoader for the validation data
     def get_val_dataloader(self, batch_size=32, shuffle=False):
-        """Returns a DataLoader for the validation data."""
         return DataLoader(self.val_data, batch_size=batch_size, shuffle=shuffle)
 
 
